fa13f0cba4f4d23ca40257ab32c9a45493d55dd0
raulpacheco2k <eu@raulpacheco.com.br>
2022-03-04 13:32:10 -0300

    DAO Seller

--- src/model/bo/Seller.java
+++ src/model/bo/Seller.java
@@ -8,7 +8,7 @@
 
 package model.bo;
 
-public class Seller {
+public class Seller extends Model {
     private int id;
     private String name;
     private String cpf;
@@ -18,12 +18,13 @@ public class Seller {
     private float salesCommissionPercentage;
     private float paymentCommissionPercentage;
     private String completeAddress;
-    private String address_id;
+    private int address_id;
 
     public Seller() {
+        setTable("vendedor");
     }
 
-    public Seller(int id, String name, String cpf, String email, String phone1, String phone2, float salesCommissionPercentage, float paymentCommissionPercentage, String completeAddress, String address_id) {
+    public Seller(int id, String name, String cpf, String email, String phone1, String phone2, float salesCommissionPercentage, float paymentCommissionPercentage, String completeAddress, int address_id) {
         this.id = id;
         this.name = name;
         this.cpf = cpf;
@@ -34,6 +35,7 @@ public class Seller {
         this.paymentCommissionPercentage = paymentCommissionPercentage;
         this.completeAddress = completeAddress;
         this.address_id = address_id;
+        setTable("vendedor");
     }
 
     public int getId() {
@@ -117,12 +119,42 @@ public class Seller {
         return this;
     }
 
-    public String getAddress_id() {
+    public int getAddress_id() {
         return address_id;
     }
 
-    public Seller setAddress_id(String address_id) {
+    public Seller setAddress_id(int address_id) {
         this.address_id = address_id;
         return this;
+    }
+
+    @Override
+    public String insert() {
+        return "INSERT INTO vendedor (nomeVendedor, cpfVendedor, emailVendedor, foneVendedor, fone2Vendedor, percentComissaoVenda, percentComissaoRecebto, compleEnderecoVendedor, endereco_idcep) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    }
+
+    @Override
+    public String findAll() {
+        return "SELECT idvendedor, nomeVendedor, cpfVendedor, emailVendedor, foneVendedor, fone2Vendedor, percentComissaoVenda, percentComissaoRecebto, compleEnderecoVendedor, endereco_idcep FROM vendedor";
+    }
+
+    @Override
+    public String findById() {
+        return "SELECT idvendedor, nomeVendedor, cpfVendedor, emailVendedor, foneVendedor, fone2Vendedor, percentComissaoVenda, percentComissaoRecebto, compleEnderecoVendedor, endereco_idcep FROM vendedor WHERE idvendedor = ?";
+    }
+
+    @Override
+    public String findByField(String string) {
+        return "SELECT idvendedor, nomeVendedor, cpfVendedor, emailVendedor, foneVendedor, fone2Vendedor, percentComissaoVenda, percentComissaoRecebto, compleEnderecoVendedor, endereco_idcep FROM vendedor WHERE " + string + " = ?";
+    }
+
+    @Override
+    public String update() {
+        return "UPDATE vendedor SET nomeVendedor = ?, cpfVendedor = ?, emailVendedor = ?, foneVendedor = ?, fone2Vendedor = ?, percentComissaoVenda = ?, percentComissaoRecebto = ?, compleEnderecoVendedor = ?, endereco_idcep = ? WHERE idvendedor = ? ";
+    }
+
+    @Override
+    public String delete() {
+        return null;
     }
 }

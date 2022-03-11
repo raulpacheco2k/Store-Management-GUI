cf90bb88cfe5b5f5716e30defb19758f8adc286d
raulpacheco2k <eu@raulpacheco.com.br>
2022-03-11 20:06:35 -0300

    Services

---
+++ src/service/BrandService.java
@@ -0,0 +1,46 @@
+package service;
+
+import model.DAO.BrandDAO;
+import model.bo.Brand;
+
+import java.util.List;
+
+public class BrandService implements InterfaceService<Brand> {
+
+    @Override
+    public void salvar(Brand objeto) {
+        BrandDAO model = new BrandDAO();
+        model.create(objeto);
+    }
+
+    @Override
+    public List<Brand> buscar() {
+        BrandDAO model = new BrandDAO();
+        return model.retrieve();
+    }
+
+    @Override
+    public Brand buscar(int codigo) {
+        BrandDAO model = new BrandDAO();
+        return model.retrieve(codigo);
+    }
+
+    @Override
+    public Brand buscar(String descricao) {
+        BrandDAO model = new BrandDAO();
+        return model.retrieve(descricao);
+    }
+
+    @Override
+    public void atualizar(Brand objeto) {
+        BrandDAO model = new BrandDAO();
+        model.update(objeto);
+    }
+
+    @Override
+    public void apagar(Brand objeto) {
+        BrandDAO model = new BrandDAO();
+        model.delete(objeto);
+    }
+
+}

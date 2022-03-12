baef8f64f5bd798c093a416a2590572aa045f082
raulpacheco2k <eu@raulpacheco.com.br>
2022-03-12 15:31:39 -0300

    CRUD de Tipo de Produto

---
+++ src/controller/ControllerCadProductType.java
@@ -0,0 +1,90 @@
+package controller;
+
+import model.bo.ProductType;
+import service.ProductTypeService;
+import view.TelaBusProductType;
+import view.TelaCadProductType;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public final class ControllerCadProductType extends BaseController implements ActionListener {
+
+    TelaCadProductType screen;
+    ProductTypeService service;
+    public static int code = 0;
+
+    public ControllerCadProductType(TelaCadProductType screen) {
+        this.screen = screen;
+        this.service = new ProductTypeService();
+        addListener();
+        creationState(this.screen, false);
+        enableFieldsForCreation(this.screen, false);
+    }
+
+    public void addListener() {
+        screen.getjButtonBuscar().addActionListener(this);
+        screen.getjButtonNovo().addActionListener(this);
+        screen.getjButtonCancelar().addActionListener(this);
+        screen.getjButtonGravar().addActionListener(this);
+        screen.getjButtonSair().addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent acao) {
+        if (acao.getSource() == screen.getjButtonNovo()) {
+            news();
+        } else if (acao.getSource() == screen.getjButtonCancelar()) {
+            super.cancel(this.screen);
+        } else if (acao.getSource() == screen.getjButtonGravar()) {
+            store();
+        } else if (acao.getSource() == screen.getjButtonBuscar()) {
+            search();
+        } else if (acao.getSource() == screen.getjButtonSair()) {
+            exit();
+        }
+    }
+
+    public void news() {
+        creationState(this.screen, true);
+        enableFieldsForCreation(this.screen, true);
+        this.screen.getId().setEnabled(false);
+    }
+
+    public void store() {
+        ProductType model = new ProductType();
+        model.setDescription(this.screen.getNome().getText());
+
+        if (this.screen.getId().getText().trim().equalsIgnoreCase("")) {
+            this.service.salvar(model);
+        } else {
+            model.setId(Integer.parseInt(this.screen.getId().getText()));
+            this.service.atualizar(model);
+        }
+
+        creationState(this.screen, false);
+        enableFieldsForCreation(this.screen, false);
+    }
+
+    public void search() {
+        TelaBusProductType telaBusProductType = new TelaBusProductType(null, true);
+        ControllerBusProductType controllerBusProductType = new ControllerBusProductType(telaBusProductType);
+        telaBusProductType.setVisible(true);
+
+        if (code != 0) {
+            ProductType model;
+            model = this.service.buscar(code);
+
+            creationState(this.screen, true);
+            enableFieldsForCreation(this.screen, true);
+
+            this.screen.getId().setText(model.getId() + "");
+            this.screen.getNome().setText(model.getDescription());
+            this.screen.getId().setEnabled(false);
+        }
+    }
+
+    public void exit() {
+        screen.dispose();
+    }
+}
